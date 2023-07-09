3577e829bab5e9da88168c4ebcd7e119c17962ea
sethbarryberg <bergseth98@gmail.com>
2023-07-09 09:36:06 -0700

    V1

---
+++ fall_distance.py
@@ -0,0 +1,11 @@
+#Remember that, as stated in the Code Style Guidelines, all functions in all assignments must have a docstring.
+def fall_distance(t):
+    """This function calculated fall distance in meters by having the user input time falling (in seconds)"""
+    d = (1/2)*9.8*(t)**2
+    return d
+
+#print(fall_distance(3.2))
+
+
+
+
